39d278921ba5c75109ad38976dfd94318daa1198
Britton Smith <brittonsmith@gmail.com>
2016-01-16 11:41:46 +0000

    Refactoring to remove SimpleHalo class.

--- simple_tree.py
+++ simple_tree.py
@@ -20,22 +20,13 @@ def simple_ancestry(hc, candidate):
     common = np.intersect1d(hc_ids, c_ids)
     return common.size > threshold * c_ids.size
 
-class SimpleHalo(object):
-    def __init__(self, halo_type, halo_id, halo_info=None):
-        self.halo_type = halo_type
-        self.halo_id = halo_id
-        self.ancestors = None
-        if halo_info is None:
-            halo_info = {}
-        for key, value in halo_info.items():
-            setattr(self, key, value)
-
 class SimpleTree(object):
     def __init__(self, time_series):
         self.ts = time_series
 
-    def find_ancestors(self, hc, ds2):
+    def find_ancestors(self, halo_type, halo_id, ds1, ds2):
         ancestors = []
+        hc = ds1.halo(halo_type, halo_id)
         candidate_ids = halos_in_sphere(hc, ds2, "Group_R_Crit200", 5)
         for candidate_id in candidate_ids:
             candidate = ds2.halo(hc.ptype, candidate_id)
@@ -48,33 +39,36 @@ class SimpleTree(object):
         ds1 = yt.load(outputs_r[0])
         halo_info = {"redshift": ds1.current_redshift,
                      "ds": ds1.parameter_filename}
-        start_halos = [SimpleHalo(halo_type, halo_id, halo_info)
-                       for halo_id in halo_ids]
-        current_halos = start_halos
+        current_ids = halo_ids
+
+        all_ancestor_ids = []
+        all_descendent_ids = []
 
         for fn in outputs_r[1:]:
-            next_halos = []
             ds2 = yt.load(fn)
 
             yt.mylog.info("Searching for ancestors of %d halos." %
-                          len(current_halos))
+                          len(current_ids))
 
             halo_info["redshift"] = ds2.current_redshift
             halo_info["ds"] = ds2.parameter_filename
 
-            for current_halo in current_halos:
-                hc = ds1.halo(current_halo.halo_type,
-                              current_halo.halo_id)
-                ancestor_ids = self.find_ancestors(hc, ds2)
-                ancestor_halos = \
-                  [SimpleHalo(halo_type, ancestor_id, halo_info)
-                   for ancestor_id in ancestor_ids]
-                current_halo.ancestors = ancestor_halos
-                next_halos.extend(ancestor_halos)
+            these_ancestor_ids = []
+            these_descendent_ids = []
+
+            for current_id in current_ids:
+                ancestor_ids = self.find_ancestors(
+                    halo_type, current_id, ds1, ds2)
+                these_ancestor_ids.extend(ancestor_ids)
+                these_descendent_ids.extend(
+                    [current_id]*len(ancestor_ids))
+
+            all_ancestor_ids.append(these_ancestor_ids)
+            all_descendent_ids.append(these_descendent_ids)
 
             ds1 = ds2
-            current_halos = next_halos
-            if len(current_halos) == 0:
+            current_ids = these_ancestor_ids
+            if len(current_ids) == 0:
                 break
 
-        return start_halos
+        return # need to return something
