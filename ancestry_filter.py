54515f2fcebbd320882a7504aa2159d172a17a90
Britton Smith <brittonsmith@gmail.com>
2016-01-21 10:53:52 +0000

    Adding an ancestry filter and ability to get other halo properties by returning halo containers from ancestry check.

---
+++ ancestry_filter.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+from yt.utilities.operator_registry import \
+    OperatorRegistry
+
+ancestry_filter_registry = OperatorRegistry()
+
+def add_ancestry_filter(name, function):
+    ancestry_filter_registry[name] = AncestryFilter(function)
+
+class AncestryFilter(object):
+    r"""
+    An AncestryFilter takes a halo and a list of ancestors and
+    returns a filtered list of filtered list of ancestors.  For
+    example, a filter could return only the most massive ancestor.
+
+    Required Arguments
+    ------------------
+    halo: halo data container
+        Data container of the descendent halo.
+    ancestors : list of halo data containers
+        List of data containers for the ancestor halos.
+
+    The function should return a list of data containers.
+
+    """
+    def __init__(self, function, args=None, kwargs=None):
+        self.function = function
+        self.args = args
+        if self.args is None: self.args = []
+        self.kwargs = kwargs
+        if self.kwargs is None: self.kwargs = {}
+
+    def __call__(self, halo, ancestors):
+        return self.function(halo, ancestors, *self.args, **self.kwargs)
+
+def most_massive(halo, ancestors):
+    r"""
+    Return only the most massive ancestor.
+
+    Parameters
+    ----------
+    halo: halo data container
+        Data container of the descendent halo.
+    ancestors : list of halo data containers
+        List of data containers for the ancestor halos.
+
+    Returns
+    -------
+    filtered_ancestors : list of halo data containers
+        List containing data container of most massive halo.
+
+    """
+    i_max = np.argmax([my_halo.mass for my_halo in ancestors])
+    return [ancestors[i_max]]
+
+add_ancestry_filter("most_massive", most_massive)
--- simple_tree.py
+++ simple_tree.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import numpy as np
 import yt
 
@@ -6,6 +7,10 @@ from yt.utilities.parallel_tools.parallel_analysis_interface import \
 
 from .ancestry_checker import \
     ancestry_checker_registry
+from .ancestry_filter import \
+    ancestry_filter_registry
+from .communication import \
+    mpi_gather_list
 from .halo_selector import \
     selector_registry
 
@@ -19,12 +24,18 @@ class SimpleTree(object):
         # set a default ancestry checker
         self.set_ancestry_checker("common_ids")
 
+        self.ancestry_filter = None
+
     def set_selector(self, selector, *args, **kwargs):
         self.selector = selector_registry.find(selector, *args, **kwargs)
 
     def set_ancestry_checker(self, ancestry_checker, *args, **kwargs):
         self.ancestry_checker = \
           ancestry_checker_registry.find(ancestry_checker, *args, **kwargs)
+
+    def set_ancestry_filter(self, ancestry_filter, *args, **kwargs):
+        self.ancestry_filter = \
+          ancestry_filter_registry.find(ancestry_filter, *args, **kwargs)
 
     def find_ancestors(self, halo_type, halo_id, ds1, ds2):
         hc = ds1.halo(halo_type, halo_id)
@@ -36,15 +47,23 @@ class SimpleTree(object):
             candidate = ds2.halo(halo_type, candidate_id)
             candidate_member_ids = candidate["member_ids"].d.astype(np.int64)
             if self.ancestry_checker(halo_member_ids, candidate_member_ids):
-                ancestors.append(candidate_id)
-        return ancestors
+                ancestors.append(candidate)
 
-    def trace_lineage(self, halo_type, halo_ids):
+        if self.ancestry_filter is not None:
+            ancestors = self.ancestry_filter(hc, ancestors)
+
+        return hc, ancestors
+
+    def trace_lineage(self, halo_type, halo_ids, halo_properties=None):
+        if halo_properties is None:
+            halo_properties = []
+
         outputs_r = self.ts.outputs[::-1]
         ds1 = yt.load(outputs_r[0])
 
         all_ancestor_counts = []
         all_halo_ids = [halo_ids]
+        all_halo_properties = defaultdict(list)
 
         for fn in outputs_r[1:]:
             ds2 = yt.load(fn)
@@ -55,21 +74,37 @@ class SimpleTree(object):
 
             these_ancestor_ids = []
             these_ancestor_counts = []
+            these_halo_properties = \
+              dict([(halo_property, []) for halo_property in halo_properties])
 
             comm = _get_comm(())
             njobs = min(comm.size, len(all_halo_ids[-1]))
             for current_id in yt.parallel_objects(all_halo_ids[-1], njobs=-1):
-                ancestor_ids = self.find_ancestors(
+                current_halo, ancestors = self.find_ancestors(
                     halo_type, current_id, ds1, ds2)
-                these_ancestor_ids.extend(ancestor_ids)
-                these_ancestor_counts.append(len(ancestor_ids))
+
+                for halo_property in halo_properties:
+                    these_halo_properties[halo_property].extend(
+                        [get_halo_property(ancestor, halo_property)
+                         for ancestor in ancestors])
+
+                these_ancestor_ids.extend([ancestor.particle_identifier
+                                           for ancestor in ancestors])
+                these_ancestor_counts.append(len(ancestors))
 
             these_ancestor_ids = comm.par_combine_object(
                 these_ancestor_ids, "cat", datatype="list")
             these_ancestor_counts = comm.par_combine_object(
                 these_ancestor_counts, "cat", datatype="list")
+            if comm.comm is not None:
+                for halo_property in these_halo_properties:
+                    these_halo_properties[halo_property] = \
+                      mpi_gather_list(comm.comm, these_halo_properties[halo_property])
 
             all_ancestor_counts.append(these_ancestor_counts)
+            for halo_property in these_halo_properties:
+                all_halo_properties[halo_property].append(
+                    these_halo_properties[halo_property])
             if len(these_ancestor_ids) > 0:
                 all_halo_ids.append(these_ancestor_ids)
             else:
@@ -77,3 +112,8 @@ class SimpleTree(object):
             ds1 = ds2
 
         return # need to return something
+
+def get_halo_property(halo, halo_property):
+    val = getattr(halo, halo_property, None)
+    if val is None: val = halo[halo_property]
+    return val
