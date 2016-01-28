e269797d503781d4dbddfec15ea829cb7a678797
Britton Smith <brittonsmith@gmail.com>
2016-01-28 12:14:01 +0000

    Adding progress bars for loading.

--- simple_knot.py
+++ simple_knot.py
@@ -23,14 +23,18 @@ def load_tree(filename, properties=None):
     level_count = []
     i_count = 0
     knots_on_level = 1
+    pbar = yt.get_pbar("Calculating levels", ancestor_counts.size)
     while (i_count < ancestor_counts.size):
         level_count.append(ancestor_counts[i_count:i_count+knots_on_level])
         i_count += knots_on_level
         knots_on_level = level_count[-1].sum()
+        pbar.update(i_count)
+    pbar.finish()
 
     # 2) Create level arrays full of knots.
     i_halo = 0
     level_knots = []
+    pbar = yt.get_pbar("Creating knots", halo_ids.size)
     for i_level, level in enumerate(level_count):
         level_knots.append([])
         for i_on_level in range(sum(level)):
@@ -41,15 +45,20 @@ def load_tree(filename, properties=None):
             level_knots[-1].append(SimpleKnot(halo_ids[i_halo],
                                               halo_properties))
             i_halo += 1
+            pbar.update(i_halo)
+    pbar.finish()
     level_count.pop(0)
 
     # 3) Assemble tree structure.
+    pbar = yt.get_pbar("Assembling tree", len(level_knots[:-1]))
     for i_level, knots in enumerate(level_knots[:-1]):
         i_anc = 0
         for i_knot, knot in enumerate(knots):
             n_anc = level_count[i_level][i_knot]
             knot.ancestors = level_knots[i_level+1][i_anc:i_anc+n_anc]
             i_anc += n_anc
+        pbar.update(i_level)
+    pbar.finish()
     
     my_tree = level_knots.pop(0)
     del level_knots
