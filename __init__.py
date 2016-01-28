decaf18db934963dc17139a34e394322308e4da0
Britton Smith <brittonsmith@gmail.com>
2016-01-28 12:38:35 +0000

    Adding function to return ancestry arrays.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from simple_tree import \
     SimpleTree
 from simple_knot import \
-    load_tree
+    load_tree, \
+    get_ancestry_arrays
 from ancestry_checker import \
     add_ancestry_checker
 from halo_selector import \
--- simple_knot.py
+++ simple_knot.py
@@ -4,13 +4,28 @@ import yt
 class SimpleKnot(object):
     def __init__(self, halo_id, properties=None):
         self.halo_id = halo_id
-        self.ancestors = []
+        self.ancestors = None
         if properties is None: properties = {}
         for p, val in properties.items():
             setattr(self, p, val)
 
     def __repr__(self):
         return "SimpleKnot[%d]" % self.halo_id
+
+def get_ancestry_arrays(knot, fields):
+    data = dict([(field, []) for field in fields])
+    my_knot = knot
+    while my_knot is not None:
+        for field in fields:
+            data[field].append(getattr(my_knot, field))
+        if my_knot.ancestors is not None and \
+          len(my_knot.ancestors) > 0:
+            my_knot = my_knot.ancestors[0]
+        else:
+            my_knot = None
+    for field in data:
+        data[field] = yt.YTArray(data[field])
+    return data
 
 def load_tree(filename, properties=None):
     ds = yt.load(filename)
