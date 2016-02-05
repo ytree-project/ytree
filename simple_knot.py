9662200f900af1718751523e9cff8dfc9171d07d
Britton Smith <brittonsmith@gmail.com>
2016-02-05 12:23:12 +0000

    Adding new load_tree.

--- simple_knot.py
+++ simple_knot.py
@@ -4,16 +4,22 @@ import numpy as np
 import os
 import yt
 
-class SimpleKnot(object):
-    def __init__(self, halo_id, properties=None):
+class TreeNode(object):
+    def __init__(self, halo_id, level_id=None):
         self.halo_id = halo_id
+        self.level_id = level_id
         self.ancestors = None
-        if properties is None: properties = {}
-        for p, val in properties.items():
-            setattr(self, p, val)
+
+    def add_ancestor(self, ancestor):
+        if self.ancestors is None:
+            self.ancestors = []
+        self.ancestors.append(ancestor)
 
     def __repr__(self):
-        return "SimpleKnot[%d]" % self.halo_id
+        if self.level_id is None:
+            return "TreeNode[%d]" % self.halo_id
+        else:
+            return "TreeNode[%d,%d]" % (self.level_id, self.halo_id)
 
 def get_ancestry_arrays(knot, fields):
     data = dict([(field, []) for field in fields])
@@ -35,4 +41,28 @@ def load_tree(output_dir, properties=None):
     my_files.sort()
     if properties is None: properties = {}
 
-    
+    my_tree = None
+    for i, fn in enumerate(my_files):
+        fh = h5py.File(fn, "r")
+        if my_tree is None:
+            des_ids = fh["data/descendent_particle_identifier"].value
+        else:
+            des_ids = anc_ids
+        anc_ids = fh["data/ancestor_particle_identifier"].value
+        links = fh["data/links"].value
+        fh.close()
+
+        if my_tree is None:
+            des_nodes = [TreeNode(my_id, i) for my_id in des_ids]
+            my_tree = des_nodes
+        else:
+            des_nodes = anc_nodes
+
+        anc_nodes = [TreeNode(my_id, i+1) for my_id in anc_ids]
+
+        for link in links:
+            i_des = np.where(link[0] == des_ids)[0][0]
+            i_anc = np.where(link[1] == anc_ids)[0][0]
+            des_nodes[i_des].add_ancestor(anc_nodes[i_anc])
+
+    return my_tree
