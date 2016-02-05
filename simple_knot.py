311e5a80625be7399942ff19346d3652c941fcdb
Britton Smith <brittonsmith@gmail.com>
2016-02-05 11:40:30 +0000

    Removing old tree loading and starting anew.

--- simple_knot.py
+++ simple_knot.py
@@ -1,4 +1,7 @@
+import h5py
+import glob
 import numpy as np
+import os
 import yt
 
 class SimpleKnot(object):
@@ -27,54 +30,9 @@ def get_ancestry_arrays(knot, fields):
         data[field] = yt.YTArray(data[field])
     return data
 
-def load_tree(filename, properties=None):
-    ds = yt.load(filename)
+def load_tree(output_dir, properties=None):
+    my_files = glob.glob(os.path.join(output_dir, "tree_segment_*.h5"))
+    my_files.sort()
     if properties is None: properties = {}
 
-    halo_ids = ds.data["halo_ids"].d.astype(np.int64)
-    ancestor_counts = ds.data["ancestor_counts"].d.astype(np.int64)
-
-    # 1) Get the number of ancestors on each level.
-    level_count = []
-    i_count = 0
-    knots_on_level = 1
-    pbar = yt.get_pbar("Calculating levels", ancestor_counts.size)
-    while (i_count < ancestor_counts.size):
-        level_count.append(ancestor_counts[i_count:i_count+knots_on_level])
-        i_count += knots_on_level
-        knots_on_level = level_count[-1].sum()
-        pbar.update(i_count)
-    pbar.finish()
-
-    # 2) Create level arrays full of knots.
-    i_halo = 0
-    level_knots = []
-    pbar = yt.get_pbar("Creating knots", halo_ids.size)
-    for i_level, level in enumerate(level_count):
-        level_knots.append([])
-        for i_on_level in range(sum(level)):
-            halo_properties = dict([(hp, ds.data[hp][i_halo])
-                                    for hp in properties])
-            halo_properties["redshift"] = ds.data["redshift"][i_level]
-            my_id = halo_ids[i_halo]
-            level_knots[-1].append(SimpleKnot(halo_ids[i_halo],
-                                              halo_properties))
-            i_halo += 1
-            pbar.update(i_halo)
-    pbar.finish()
-    level_count.pop(0)
-
-    # 3) Assemble tree structure.
-    pbar = yt.get_pbar("Assembling tree", len(level_knots[:-1]))
-    for i_level, knots in enumerate(level_knots[:-1]):
-        i_anc = 0
-        for i_knot, knot in enumerate(knots):
-            n_anc = level_count[i_level][i_knot]
-            knot.ancestors = level_knots[i_level+1][i_anc:i_anc+n_anc]
-            i_anc += n_anc
-        pbar.update(i_level)
-    pbar.finish()
     
-    my_tree = level_knots.pop(0)
-    del level_knots
-    return my_tree
