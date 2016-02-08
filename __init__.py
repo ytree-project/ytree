fb1be16defd8f443dc0b7014a814e6fb9b4f98b9
Britton Smith <brittonsmith@gmail.com>
2016-02-08 14:35:41 +0000

    Renaming simple_knot to arbor.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
+from arbor import \
+    Arbor
 from tree_farm import \
     TreeFarm
-from simple_knot import \
-    Arbor
 from ancestry_checker import \
     add_ancestry_checker
 from halo_selector import \
