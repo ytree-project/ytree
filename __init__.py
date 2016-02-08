3b510eec579b6eb69110af9e5951049b84b5ab43
Britton Smith <brittonsmith@gmail.com>
2016-02-08 14:38:19 +0000

    Adding to imports.

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from tree_farm import \
     TreeFarm
 from ancestry_checker import \
     add_ancestry_checker
+from ancestry_filter import \
+    add_ancestry_filter
 from ancestry_short import \
     add_ancestry_short
 from halo_selector import \
