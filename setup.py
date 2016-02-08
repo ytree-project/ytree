5b14f289ea3e11028a1eb5ebd9728e3bbfdee2ad
Britton Smith <brittonsmith@gmail.com>
2016-02-08 17:11:21 +0000

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name="simple_tree",
+      version="1.0",
+      description="A simple merger tree for Gadget_FOF data in yt.",
+      author="Britton Smith",
+      author_email="brittonsmith@gmail.com",
+      license="BSD",
+      url="http://bitbucket.org/brittonsmith/simple_tree",
+      packages=["simple_tree"],
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Console",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: POSIX :: AIX",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Utilities",
+          ],
+      )
