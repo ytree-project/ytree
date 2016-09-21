dd3d9b35b8a58a0b5d753a5d8c8cfbc244240aa2
Britton Smith <brittonsmith@gmail.com>
2016-09-21 16:50:44 +0100

    Add header.

--- ytree/config.py
+++ ytree/config.py
@@ -1,3 +1,18 @@
+"""
+ytree config
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016, Britton Smith <brittonsmith@gmail.com>
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 import configparser
 import os
 
