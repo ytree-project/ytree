0af9babcb0a34242301e0b757d78d523ffdb8d83
Britton Smith <brittonsmith@gmail.com>
2016-09-24 12:13:10 +0100

    Adding docstrings for TreeFarm.

--- ytree/tree_farm.py
+++ ytree/tree_farm.py
@@ -35,6 +35,60 @@ from ytree.halo_selector import \
     clear_id_cache
 
 class TreeFarm(object):
+    r"""
+    TreeFarm is the merger-tree creator for Gadget FoF and Subfind
+    halo catalogs.
+
+    TreeFarm can be used to create a merger-tree for the full set of
+    halos, starting from the first catalog, or can be used to trace the
+    ancestry of specific halos, starting from the last catalog.  The
+    merger-tree process will create a new set of halo catalogs,
+    containing necessary fields (positions, velocities, masses),
+    user-requested fields, and descendent IDs for each halo.  These
+    halo catalogs can be loaded at yt datasets.
+
+    Parameters
+    ----------
+    time_series : yt DatasetSeries object
+        A yt time-series object containing the datasets over which
+        the merger-tree will be calculated.
+    setup_function : optional, callable
+        A function that accepts a yt Dataset object and performs any
+        setup, such as adding derived fields.
+
+    Examples
+    --------
+
+    To create a full merger tree:
+
+    >>> import nummpy as np
+    >>> import yt
+    >>> import ytree
+    >>> ts = yt.DatasetSeries("data/groups_*/fof_subhalo_tab*.0.hdf5")
+    >>> my_tree = ytree.TreeFarm(ts)
+    >>> my_tree.trace_descendents("Group", filename="all_halos/")
+    >>> a = ytree.load("all_halos/fof_subhalo_tab_000.0.hdf5.0.h5")
+    >>> m = a.arr([t["particle_mass"] for t in a.trees])
+    >>> i = np.argmax(m)
+    >>> print (a.trees[i].line("particle_mass").to("Msun/h"))
+
+    To create a merger tree for a specific halo or set of halos:
+
+    >>> import nummpy as np
+    >>> import yt
+    >>> import ytree
+    >>> ts = yt.DatasetSeries("data/groups_*/fof_subhalo_tab*.0.hdf5")
+    >>> ds = yt[-1]
+    >>> i = np.argmax(ds.r["Group", "particle_mass"].d)
+    >>> my_ids = ds.r["Group", "particle_identifier"][i_max]
+    >>> my_tree = ytree.TreeFarm(ts)
+    >>> my_tree.set_ancestry_filter("most_massive")
+    >>> my_tree.set_ancestry_short("above_mass_fraction", 0.5)
+    >>> my_tree.trace_ancestors("Group", my_ids, filename="my_halos/")
+    >>> a = ytree.load("my_halos/fof_subhalo_tab_025.0.hdf5.0.h5")
+    >>> print (a.trees[0].line("particle_mass").to("Msun/h"))
+
+    """
     def __init__(self, time_series, setup_function=None):
         self.ts = time_series
         self.setup_function = setup_function
@@ -50,27 +104,83 @@ class TreeFarm(object):
         self.comm = _get_comm(())
 
     def set_selector(self, selector, *args, **kwargs):
+        r"""
+        Set the method for selecting candidate halos for tracing
+        halo ancestry.
+
+        The default selector is "all", i.e., check every halo for a
+        possible match.  This can be slow.  The "sphere" selector
+        can be used to specify that only halos within some sphere
+        be checked.
+
+        Parameters
+        ----------
+        selector : string
+            Name of selector.
+        """
         self.selector = selector_registry.find(selector, *args, **kwargs)
 
     def set_ancestry_checker(self, ancestry_checker, *args, **kwargs):
+        r"""
+        Set the method for determing if a halo is the ancestor of
+        another halo.
+
+        The default method defines an ancestor as a halo where at least
+        50% of its particles are found in the descendent.
+
+        Parameters
+        ----------
+        ancestry_checker : string
+            Name of checking method.
+        """
         self.ancestry_checker = \
           ancestry_checker_registry.find(ancestry_checker, *args, **kwargs)
 
     def set_ancestry_filter(self, ancestry_filter, *args, **kwargs):
+        r"""
+        Select a method for determining which ancestors are kept.
+        The kept ancestors will have their ancestries tracked.  This
+        can be used to speed up merger-trees for targeted halos by
+        specifying that only the most massive ancestor be kept.
+
+        Parameters
+        ----------
+        ancestry_filter : string
+            Name of filter method.
+        """
         self.ancestry_filter = \
           ancestry_filter_registry.find(ancestry_filter, *args, **kwargs)
 
     def set_ancestry_short(self, ancestry_short, *args, **kwargs):
+        r"""
+        Select a method for cutting short the ancestry search.
+
+        This can be used to speed up merger-trees for targeted halos by
+        specifying that the search come to an end when an ancestor with
+        greater than 50% of the halo's mass has been found, thereby
+        ensuring that the most massive halo has already been found.
+
+        Parameters
+        ----------
+        ancestry_short : string
+            Name of short-out method.
+        """
         self.ancestry_short = \
           ancestry_short_registry.find(ancestry_short, *args, **kwargs)
 
     def _load_ds(self, filename):
+        """
+        Load a catalog as a yt dataset and call setup function.
+        """
         ds = yt.load(filename)
         if self.setup_function is not None:
             self.setup_function(ds)
         return ds
 
     def _find_ancestors(self, hc, ds2, id_store=None):
+        """
+        Search for ancestors of a given halo.
+        """
         if id_store is None: id_store = []
         halo_member_ids = hc["member_ids"].d.astype(np.int64)
         candidate_ids = self.selector(hc, ds2)
@@ -96,6 +206,9 @@ class TreeFarm(object):
         return ancestors
 
     def _find_descendent(self, hc, ds2):
+        """
+        Search for descendents of a given halo.
+        """
         halo_member_ids = hc["member_ids"].d.astype(np.int64)
         candidate_ids = self.selector(hc, ds2)
 
@@ -109,6 +222,25 @@ class TreeFarm(object):
 
     def trace_ancestors(self, halo_type, root_ids,
                         halo_properties=None, filename=None):
+        """
+        Trace the ancestry of a given set of halos.
+
+        A merger-tree for a specific set of halos will be created,
+        starting with the last halo catalog and moving backward.
+
+        Parameters
+        ----------
+        halo_type : string
+            The type of halo, typically "FOF" for FoF groups or
+            "Subfind" for subhalos.
+        root_ids : integer or array of integers
+            The halo IDs from the last halo catalog for the
+            targeted halos.
+        halo_properties : optional, list of strings
+            List of additional fields to be saved to halo catalogs.
+        filename : optional, string
+            Directory in which merger-tree catalogs will be saved.
+        """
 
         output_dir = os.path.dirname(filename)
         if yt.is_root() and len(output_dir) > 0:
@@ -186,6 +318,22 @@ class TreeFarm(object):
 
     def trace_descendents(self, halo_type,
                           halo_properties=None, filename=None):
+        """
+        Trace the descendents of all halos.
+
+        A merger-tree for all halos will be created, starting
+        with the first halo catalog and moving forward.
+
+        Parameters
+        ----------
+        halo_type : string
+            The type of halo, typically "FOF" for FoF groups or
+            "Subfind" for subhalos.
+        halo_properties : optional, list of strings
+            List of additional fields to be saved to halo catalogs.
+        filename : optional, string
+            Directory in which merger-tree catalogs will be saved.
+        """
 
         output_dir = os.path.dirname(filename)
         if yt.is_root() and len(output_dir) > 0:
@@ -247,6 +395,9 @@ class TreeFarm(object):
                            halo_properties)
 
     def _save_catalog(self, filename, ds, halos, halo_properties=None):
+        """
+        Save halo catalog with descendent information.
+        """
         if self.comm is None:
             rank = 0
         else:
@@ -284,6 +435,10 @@ class TreeFarm(object):
                            extra_attrs=extra_attrs)
 
     def _create_halo_data_lists(self, halos, halo_properties):
+        """
+        Given a list of halo containers, return a dictionary
+        of field values for all halos.
+        """
         pbar = yt.get_pbar("Gathering field data from halos",
                            self.comm.size*len(halos), parallel=True)
         data = dict([(hp, []) for hp in halo_properties])
@@ -305,6 +460,10 @@ class TreeFarm(object):
         return data
 
 def _get_halo_property(halo, halo_property):
+    """
+    Convenience function for querying fields and
+    other properties from halo containers.
+    """
     val = getattr(halo, halo_property, None)
     if val is None: val = halo[halo_property]
     return val
