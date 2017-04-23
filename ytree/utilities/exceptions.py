2c806df16dca450e9f99a2b3edf3f81f99df7946
Britton Smith <brittonsmith@gmail.com>
2017-04-23 16:43:15 -0500

    Add some exceptions.

---
+++ ytree/utilities/exceptions.py
@@ -0,0 +1,29 @@
+"""
+ytree exceptions
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, Britton Smith <brittonsmith@gmail.com>
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+class ArborFieldException(Exception):
+    def __init__(self, field, arbor=None):
+        self.field = field
+        self.arbor = arbor
+
+class ArborFieldCircularDependency(ArborFieldException):
+    def __str__(self):
+        return "Field depends on itself: \"%s\" in %s." % \
+          (self.field, self.arbor)
+
+class ArborFieldNotFound(ArborFieldException):
+    def __str__(self):
+        return "Field not found: \"%s\" in %s." % \
+          (self.field, self.arbor)
