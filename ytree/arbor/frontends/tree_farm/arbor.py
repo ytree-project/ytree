2e8e57fa2e67e60fe0a8e8d89ee2d936865ce388
Britton Smith <brittonsmith@gmail.com>
2017-07-08 18:11:39 -0700

    Updating TreeFarmArbor.

--- ytree/arbor/frontends/tree_farm/arbor.py
+++ ytree/arbor/frontends/tree_farm/arbor.py
@@ -6,7 +6,7 @@ TreeFarmArbor class and member functions
 """
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2016, Britton Smith <brittonsmith@gmail.com>
+# Copyright (c) 2016-2017, Britton Smith <brittonsmith@gmail.com>
 #
 # Distributed under the terms of the Modified BSD License.
 #
@@ -17,85 +17,63 @@ import glob
 import h5py
 import numpy as np
 
-from yt.convenience import \
-    load as yt_load
-from yt.units.dimensions import \
-    length
+from yt.units.unit_registry import \
+    UnitRegistry
 
 from ytree.arbor.arbor import \
     CatalogArbor
+from ytree.arbor.frontends.tree_farm.fields import \
+    TreeFarmFieldInfo
+from ytree.arbor.frontends.tree_farm.io import \
+    TreeFarmDataFile
+from ytree.utilities.io import \
+    _hdf5_yt_attr
 
 class TreeFarmArbor(CatalogArbor):
     """
     Class for Arbors created with :class:`~ytree.tree_farm.TreeFarm`.
     """
-    def _set_default_selector(self):
-        """
-        Mass is "particle_mass".
-        """
-        self.set_selector("max_field_value", "particle_mass")
 
-    def _get_all_files(self):
-        """
-        Get all files and put in reverse order.
-        """
-        prefix = self.filename.rsplit("_", 1)[0]
-        suffix = ".h5"
-        my_files = glob.glob("%s_*%s" % (prefix, suffix))
-        # sort by catalog number
-        my_files.sort(key=lambda x:
-                      int(x[x.find(prefix)+len(prefix)+1:x.find(".0")]),
-                      reverse=True)
-        return my_files
+    _suffix = ".h5"
+    _field_info_class = TreeFarmFieldInfo
+    _data_file_class = TreeFarmDataFile
 
-    def _load_field_data(self, fn, offset):
-        """
-        Load field data as a yt HaloCatalog dataset.
-        Get cosmological parameters and modify unit registry
-        for hubble constant.
-        Create a redshift field and uid field and rename
-        particle_identifier and descendent_identifier to halo_id
-        and desc_id.
-        """
-        ds = yt_load(fn)
+    def _parse_parameter_file(self):
+        fh = h5py.File(self.filename, "r")
 
-        if not hasattr(self, "hubble_constant"):
-            for attr in ["hubble_constant",
-                         "omega_matter",
-                         "omega_lambda"]:
-                setattr(self, attr, getattr(ds, attr))
-            # Drop the "cm" suffix because all lengths will
-            # be in comoving units.
-            self.box_size = self.quan(
-                ds.domain_width[0].to("Mpccm/h"), "Mpc/h")
-        try:
-            if len(ds.field_list) == 0 or ds.index.total_particles == 0:
-                return 0
-        except ValueError:
-            return 0
-        skip_fields = ["particle_identifier", "descendent_identifier"]
-        add_fields = [("halos", field)
-                      for field in ["particle_position",
-                                    "particle_velocity"]]
-        for field in ds.field_list + add_fields:
-            if field[0] != "halos": continue
-            if "particle_position_" in field[1]: continue
-            if "particle_velocity_" in field[1]: continue
-            if field[1] in skip_fields: continue
-            field_data = ds.r[field]
-            if field_data.units.dimensions is length:
-                field_data.convert_to_units("unitary")
-            self._field_data[field[1]].append(field_data)
-        self._field_data["halo_id"].append(
-          ds.r["halos", "particle_identifier"].d.astype(np.int64))
-        self._field_data["desc_id"].append(
-          ds.r["halos", "descendent_identifier"].d.astype(np.int64))
-        n_halos = self._field_data["halo_id"][-1].size
-        self._field_data["redshift"].append(
-            ds.current_redshift * np.ones(n_halos))
-        self._field_data["uid"].append(
-            np.arange(offset, offset+n_halos))
-        return n_halos
+        for attr in ["hubble_constant",
+                     "omega_matter",
+                     "omega_lambda"]:
+            setattr(self, attr, fh.attrs[attr])
+
+        my_ur = UnitRegistry.from_json(
+            fh.attrs["unit_registry_json"])
+        right = _hdf5_yt_attr(fh, "domain_right_edge",
+                              unit_registry=my_ur)
+        left  = _hdf5_yt_attr(fh, "domain_left_edge",
+                              unit_registry=my_ur)
+        # Drop the "cm" suffix because all lengths will
+        # be in comoving units.
+        self.box_size = self.quan(
+            (right - left)[0].to("Mpccm/h"), "Mpc/h")
+
+        fields = list(fh.keys())
+        fi = {}
+        for field in fields:
+            units = fh[field].attrs["units"]
+            if isinstance(units, bytes):
+                units = units.decode("utf")
+            fi[field] = {"source": "file", "units": units}
+
+        fields.append("redshift")
+        fi["redshift"] = {"source": "header", "units": ""}
+
+        self.field_list = fields
+        self.field_info.update(fi)
+        self._get_data_files()
+
+    def _get_file_index(self, f, prefix, suffix):
+        return int(f[f.find(prefix)+len(prefix)+1:f.find(".0")])
 
     @classmethod
     def _is_valid(self, *args, **kwargs):
