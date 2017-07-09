3a9e605f9a1331cedd3ff5ae3678a96fa54385e8
Britton Smith <brittonsmith@gmail.com>
2017-07-08 18:09:46 -0700

    Some abstraction for CatalogArbors.

--- ytree/arbor/arbor.py
+++ ytree/arbor/arbor.py
@@ -16,6 +16,7 @@ Arbor class and member functions
 from collections import \
     defaultdict
 import functools
+import glob
 import json
 import numpy as np
 import os
@@ -711,11 +712,27 @@ class CatalogArbor(Arbor):
     files where the descendent ID for each halo has been
     pre-determined.
     """
+
+    _prefix = None
+    _data_file_class = None
+
     def _get_data_files(self):
         """
-        Get all input files based on specific naming convention.
+        Get all out_*.list files and sort them in reverse order.
         """
-        raise NotImplementedError
+        prefix = self.filename.rsplit("_", 1)[0]
+        suffix = self._suffix
+        my_files = glob.glob("%s_*%s" % (prefix, suffix))
+        # sort by catalog number
+        my_files.sort(
+            key=lambda x:
+            self._get_file_index(x, prefix, suffix),
+            reverse=True)
+        self.data_files = \
+          [self._data_file_class(f, self) for f in my_files]
+
+    def _get_file_index(self, f, prefix, suffix):
+        return int(f[f.find(prefix)+len(prefix)+1:f.rfind(suffix)]),
 
     def _plant_trees(self):
         fields = ["ID", "DescID"]
