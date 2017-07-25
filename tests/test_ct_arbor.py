c6721b2892a4d857d741c6fe086d7a182a77cb79
Britton Smith <brittonsmith@gmail.com>
2017-07-25 12:10:22 -0700

    Updating consistent trees test with save_tree disabled for now.

--- tests/test_ct_arbor.py
+++ tests/test_ct_arbor.py
@@ -2,45 +2,48 @@ import numpy as np
 import os
 from yt.testing import \
     requires_file
-from ytree.arbor import \
-    ArborArbor, \
-    ConsistentTreesArbor, \
-    load
+from ytree.arbor.frontends.consistent_trees import \
+    ConsistentTreesArbor
+from ytree.arbor.frontends.ytree import \
+    YTreeArbor
 from ytree.utilities.testing import \
     compare_arbors, \
-    get_test_data_dir, \
-    in_tmpdir
+    test_data_dir, \
+    TempDirTest
 
-CT = os.path.join(get_test_data_dir(),
-                  "100Mpc_64/dm_enzo/rockstar_halos/trees/tree_0_0_0.dat")
+import ytree
 
-@in_tmpdir
-@requires_file(CT)
-def test_ct_arbor():
-    a1 = load(CT)
-    assert isinstance(a1, ConsistentTreesArbor)
-    m1 = a1["mvir"]
+CT = os.path.join(test_data_dir,
+                  "rockstar_halos/trees/tree_0_0_0.dat")
 
-    fn = a1.save_arbor("arbor_ct.h5")
-    a2 = load(fn)
-    assert isinstance(a2, ArborArbor)
-    m2 = a2["mvir"]
+class CTArborTest(TempDirTest):
+    @requires_file(CT)
+    def test_ct_arbor(self):
+        a1 = ytree.load(CT)
+        assert isinstance(a1, ConsistentTreesArbor)
+        m1 = a1["mass"]
 
-    assert (m1 == m2).all()
-    compare_arbors(a1, a2)
+        fn = a1.save_arbor("arbor_ct.h5")
+        a2 = ytree.load(fn)
+        assert isinstance(a2, YTreeArbor)
+        m2 = a2["mass"]
 
-    i1 = np.argsort(m1.d)[::-1][0]
-    fn = a1[i1].save_tree()
-    a3 = load(fn)
-    assert isinstance(a3, ArborArbor)
-    for field in a1.field_list:
-        assert (a1[i1]["tree", field] == a3[0]["tree", field]).all()
+        assert (m1 == m2).all()
+        for gtype in ["tree", "prog"]:
+            compare_arbors(a2, a1, group=gtype)
 
-    # test saving trees from non-root positions
-    fn = a1[i1]["tree", 1].save_tree()
-    a4 = load(fn)
-    assert (a4[0]["tree", "tree_id"] == a4[0]["tree_id"]).all()
-    assert (a1[i1]["tree", 1]["tree", "desc_id"][1:] == a4[0]["tree", "desc_id"][1:]).all()
-    for field in a1.field_list:
-        if field in ["tree_id", "desc_id"]: continue
-        assert (a1[i1]["tree", 1]["tree", field] == a4[0]["tree", field]).all()
+        # i1 = np.argsort(m1.d)[::-1][0]
+        # fn = a1[i1].save_tree()
+        # a3 = load(fn)
+        # assert isinstance(a3, ArborArbor)
+        # for field in a1.field_list:
+        #     assert (a1[i1]["tree", field] == a3[0]["tree", field]).all()
+
+        # # test saving trees from non-root positions
+        # fn = a1[i1]["tree", 1].save_tree()
+        # a4 = load(fn)
+        # assert (a4[0]["tree", "tree_id"] == a4[0]["tree_id"]).all()
+        # assert (a1[i1]["tree", 1]["tree", "desc_id"][1:] == a4[0]["tree", "desc_id"][1:]).all()
+        # for field in a1.field_list:
+        #     if field in ["tree_id", "desc_id"]: continue
+        #     assert (a1[i1]["tree", 1]["tree", field] == a4[0]["tree", field]).all()
