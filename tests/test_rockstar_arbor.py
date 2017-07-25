fb67d003bd757ebf8fd38e16e9778c90a226d66b
Britton Smith <brittonsmith@gmail.com>
2017-07-25 14:08:31 -0700

    Renaming test files and adding test for the f_text_block function.

--- tests/test_rockstar_arbor.py
+++ tests/test_rockstar_arbor.py
@@ -4,6 +4,8 @@ from yt.testing import \
     requires_file
 from ytree.arbor.frontends.rockstar import \
     RockstarArbor
+from ytree.arbor.frontends.rockstar.misc import \
+    f_text_block
 from ytree.arbor.frontends.ytree import \
     YTreeArbor
 from ytree.utilities.testing import \
@@ -15,6 +17,8 @@ import ytree
 
 R0 = os.path.join(test_data_dir,
                   "rockstar_halos/out_0.list")
+R63 = os.path.join(test_data_dir,
+                   "rockstar_halos/out_63.list")
 
 class RockstarArborTest(TempDirTest):
     @requires_file(R0)
@@ -47,3 +51,22 @@ class RockstarArborTest(TempDirTest):
         # for field in a1.field_list:
         #     if field in ["tree_id", "desc_id"]: continue
         #     assert (a1[i1]["tree", 1]["tree", field] == a4[0]["tree", field]).all()
+
+@requires_file(R63)
+def test_f_text_block():
+    for block_size in [40, 32768]:
+        lines = []
+        locs = []
+        f = open(R63, "r")
+        for line, loc in f_text_block(f, block_size=block_size):
+            lines.append(line)
+            locs.append(loc)
+
+        lines2 = []
+        for loc in locs:
+            f.seek(loc)
+            line = f.readline()
+            lines2.append(line.strip())
+
+        for l1, l2 in zip(lines, lines2):
+            assert l1 == l2
