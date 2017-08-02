988cfb336723de62b681744d41342a49c53dae88
Britton Smith <brittonsmith@gmail.com>
2017-08-02 16:05:43 -0700

    Changing sphinx theme.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -115,19 +115,19 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'agogo'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'collapse_navigation': False}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
-#html_title = u'ytree v1.0.0'
+#html_title = u'ytree 2.0.0'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
