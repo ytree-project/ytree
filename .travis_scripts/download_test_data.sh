cf0a479775510502858817daa1edba592589481e
Britton Smith <brittonsmith@gmail.com>
2017-08-03 16:10:43 -0700

    Don't check for the base directory because the cache will get it no matter what.

--- .travis_scripts/download_test_data.sh
+++ .travis_scripts/download_test_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -d $HOME/ytree_test ]; then
+if [ ! -d $HOME/ytree_test/rockstar ]; then
     girder-cli --api-url https://girder.hub.yt/api/v1 download 59835a1ee2a67400016a2cda $HOME/ytree_test
 fi
