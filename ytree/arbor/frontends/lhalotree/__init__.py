282122140a9577daa47cc1be85508df00c360057
Britton Smith <brittonsmith@gmail.com>
2017-09-22 15:42:29 -0500

    Add template for LHaloTree frontend.

---
+++ ytree/arbor/frontends/lhalotree/__init__.py
@@ -0,0 +1,17 @@
+"""
+lhalotree frontend
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, ytree development team
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from ytree.arbor.frontends.lhalotree.arbor import \
+    LHaloTreeArbor
