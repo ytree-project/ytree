299a77f90ef0fa5528fb7410dd4d4cb872ed7133
Britton Smith <brittonsmith@gmail.com>
2017-10-22 08:51:25 -0700

    Add misc file.

---
+++ ytree/arbor/misc.py
@@ -0,0 +1,30 @@
+"""
+miscellaneous Arbor functions
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) Britton Smith <brittonsmith@gmail.com> All rights reserved.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import os
+
+from yt.funcs import \
+    ensure_dir
+
+def _determine_output_filename(path, suffix):
+    if path.endswith(suffix):
+        dirname = os.path.dirname(path)
+        filename = path[:-len(suffix)]
+    else:
+        dirname = path
+        filename = os.path.join(
+            dirname, os.path.basename(path))
+    ensure_dir(dirname)
+    return filename
