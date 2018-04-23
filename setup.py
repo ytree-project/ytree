c786342ab56b9ed986a48d6113592f8dd082c78a
Britton Smith <brittonsmith@gmail.com>
2018-04-22 17:30:25 -0700

    Missing comma.

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(name="ytree",
       extras_require={
           'dev': dev_requirements,
           'rtd': [pkg for pkg in dev_requirements if 'sphinx' not in pkg],
-      }
+      },
       python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*'
 )
