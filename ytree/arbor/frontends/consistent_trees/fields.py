2df0248983dd02e9191e747916f554c66e0166c1
Britton Smith <brittonsmith@gmail.com>
2018-08-20 14:59:57 -0700

    Add halo_id alias for consistent-trees.

--- ytree/arbor/frontends/consistent_trees/fields.py
+++ ytree/arbor/frontends/consistent_trees/fields.py
@@ -25,6 +25,7 @@ class ConsistentTreesFieldInfo(FieldInfoContainer):
     alias_fields = (
         ("uid", "id", None),
         ("desc_uid", "desc_id", None),
+        ("halo_id", "Orig_halo_ID", None),
         ("scale_factor", "scale", None),
         ("mass", "Mvir", m_unit),
         ("virial_mass", "Mvir", m_unit),
