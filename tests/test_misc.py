5a7bd1d0d24623c37614e459face3b945df94f7f
Britton Smith <brittonsmith@gmail.com>
2018-10-23 09:18:06 -0700

    Add test for PR #19 and fix data paths.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -14,13 +14,16 @@ miscellaneous utility tests
 #-----------------------------------------------------------------------------
 
 import os
+from ytree.arbor.arbor import \
+    load as ytree_load
 from ytree.utilities.io import \
     f_text_block
 from ytree.utilities.testing import \
     requires_file, \
     test_data_dir
 
-R63 = os.path.join(test_data_dir, "rockstar_halos/out_63.list")
+R0 = os.path.join(test_data_dir, "rockstar/rockstar_halos/out_0.list")
+R63 = os.path.join(test_data_dir, "rockstar/rockstar_halos/out_63.list")
 
 @requires_file(R63)
 def test_f_text_block():
@@ -40,3 +43,15 @@ def test_f_text_block():
 
         for l1, l2 in zip(lines, lines2):
             assert l1 == l2
+
+@requires_file(R0)
+def test_tree_field_clobber():
+    """
+    Test for an issue where the uid and desc_uid fields would be
+    deleted if the tree was setup in the middle of getting fields.
+    Fixed in PR #19: https://github.com/brittonsmith/ytree/pull/19
+    """
+    a = ytree_load(R0)
+    t = a[a['mass'].argmax()]
+    t['prog', 'redshift']
+    t.save_tree()
