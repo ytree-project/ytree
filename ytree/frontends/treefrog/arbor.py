8da01d715c6c640ae1cebcc5442b8287d6990cc8
Britton Smith <brittonsmith@gmail.com>
2021-06-15 16:08:08 +0100

    Add initial files for treefrog frontend. Can load metadata and plant trees.

---
+++ ytree/frontends/treefrog/arbor.py
@@ -0,0 +1,144 @@
+"""
+TreeFrogArbor class and member functions
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) ytree development team. All rights reserved.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import h5py
+import glob
+import numpy as np
+import os
+import re
+
+from yt.funcs import \
+    get_pbar
+
+from ytree.data_structures.arbor import \
+    Arbor
+
+from ytree.frontends.consistent_trees.arbor import \
+    ConsistentTreesGroupArbor
+from ytree.frontends.consistent_trees.utilities import \
+    parse_ctrees_header
+from ytree.frontends.treefrog.fields import \
+    TreeFrogFieldInfo
+from ytree.frontends.treefrog.io import \
+    TreeFrogDataFile, \
+    TreeFrogRootFieldIO, \
+    TreeFrogTreeFieldIO
+from ytree.utilities.exceptions import \
+    ArborDataFileEmpty
+from ytree.utilities.logger import \
+    ytreeLogger as mylog
+
+class TreeFrogArbor(Arbor):
+    """
+    Arbors loaded from consistent-trees data converted into HDF5.
+    """
+
+    _suffix = ".hdf5"
+    _parameter_file_is_data_file = True
+    _field_info_class = TreeFrogFieldInfo
+    _root_field_io_class = TreeFrogRootFieldIO
+    _tree_field_io_class = TreeFrogTreeFieldIO
+    _node_io_attrs = ('_fi', '_si')
+
+    # _node_io_loop_prepare = ConsistentTreesGroupArbor._node_io_loop_prepare
+
+    # def _node_io_loop_start(self, data_file):
+    #     data_file.open()
+
+    # def _node_io_loop_finish(self, data_file):
+    #     data_file._field_cache.reset()
+    #     data_file.close()
+
+    def _get_data_files(self):
+        self.data_files = [TreeFrogDataFile(f"{self._prefix}{self._suffix}.{i}")
+                           for i in range(self._nfiles)]
+
+    @property
+    def _prefix(self):
+        suffix = f".foreststats{self._suffix}"
+        return self.parameter_filename[:-len(suffix)]
+
+    def _parse_parameter_file(self):
+        with h5py.File(self.parameter_filename, mode="r") as f:
+            self._nfiles = f["Header"].attrs["NFiles"]
+            self._nsnaps = f["Header"].attrs["NSnaps"]
+            self._size = f["ForestInfo"].attrs["NForests"]
+            self._fsizes = f["ForestInfo"]["NForestsPerFile"][()]
+
+        if self._nfiles < 1:
+            mylog.warning(f"Dataset {self.parameter_filename} has no data files.")
+            return
+
+        fn = f"{self._prefix}{self._suffix}.0"
+        if not os.path.exists(fn):
+            raise RuntimeError(f"Data file not found: {fn}.")
+
+        with h5py.File(fn, mode="r") as f:
+            self.hubble_constant = f["Header/Simulation"].attrs["h_val"]
+            self.omega_matter = f["Header/Simulation"].attrs["Omega_m"]
+            self.omega_lambda = f["Header/Simulation"].attrs["Omega_Lambda"]
+            self.box_size = self.quan(f["Header/Simulation"].attrs["Period"], "Mpc/h")
+            if self._nsnaps < 1:
+                mylog.warning(f"Dataset {self.parameter_filename} has no snapshots.")
+                return
+
+            field_list = list(f["Snap_000"].keys())
+
+        self.field_list = field_list
+        self.field_info.update({field: {} for field in field_list})
+
+    def _plant_trees(self):
+        if self.is_planted or self._size == 0:
+            return
+
+        with h5py.File(self.parameter_filename, mode="r") as f:
+            self._node_info['uid'] = f["ForestInfo"]["ForestIDs"][()]
+            self._node_info['_tree_size'] = f["ForestInfo"]["ForestSizes"][()]
+
+        ei = self._fsizes.cumsum()
+        si = ei - self._fsizes
+        trees = np.arange(self._size)
+        fi = np.digitize(trees, ei)
+        # number of the file each forest lives in
+        self._node_info['_fi'] = fi
+        # index within that file each forest is at
+        self._node_info['_si'] = trees - si[fi]
+
+    @classmethod
+    def _is_valid(self, *args, **kwargs):
+        """
+        Should be an hdf5 file with a few key attributes.
+        """
+        fn = args[0]
+        suffix = f".foreststats{self._suffix}"
+        if not fn.endswith(suffix):
+            return False
+        if not h5py.is_hdf5(fn):
+            return False
+
+        info = \
+          {"Header": ['FileSplittingCriteria', 'FilesSplitByForest',
+                      'HaloCatalogBaseFileName', 'NFiles', 'NSnaps'],
+          "ForestInfo": ['MaxForestFOFGroupID', 'MaxForestFOFGroupSize',
+                         'MaxForestID', 'MaxForestSize', 'NForests']}
+
+        with h5py.File(fn, mode='r') as f:
+            for group, attrs in info.items():
+                if group not in f:
+                    return f
+                for attr in attrs:
+                    if attr not in f[group].attrs:
+                        return False
+        return True
---
+++ ytree/frontends/treefrog/io.py
@@ -0,0 +1,161 @@
+"""
+TreeFrogArbor io classes and member functions
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) ytree development team. All rights reserved.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from collections import defaultdict
+import h5py
+import numpy as np
+
+from yt.funcs import \
+    get_pbar
+
+from ytree.data_structures.io import \
+    DataFile, \
+    DefaultRootFieldIO, \
+    TreeFieldIO
+
+class ChunkStore:
+    def __init__(self, chunk_size=262144):
+        self.chunk_size = chunk_size
+        self.reset()
+
+    def reset(self):
+        self.data = {}
+        self.ind = {}
+
+    def get(self, fh, field, index):
+        start, end = index
+        si, ei = self.ind.get(field, (0, 0))
+
+        if field not in self.data or ei < end or si > start:
+            si = start
+            ei = start + self.chunk_size
+            self.ind[field] = (si, ei)
+            self.data[field] = fh[field][si:ei]
+
+        data_s = start - si
+        data_e = end - si
+        return self.data[field][data_s:data_e]
+
+class TreeFrogDataFile(DataFile):
+    # def __init__(self, filename, linkname):
+    #     super(TreeFrogDataFile, self).__init__(filename)
+    #     self.linkname = linkname
+    #     self.real_fh = None
+    #     self._field_cache = ChunkStore()
+
+    def open(self):
+        self.fh = h5py.File(self.filename, mode="r")
+
+    def close(self):
+        self.fh.close()
+        self.fh = None
+
+class TreeFrogTreeFieldIO(TreeFieldIO):
+    def _read_fields(self, root_node, fields, dtypes=None,
+                     root_only=False):
+        """
+        Read fields from disk for a single tree.
+        """
+
+        data_file = self.arbor.data_files[root_node._fi]
+
+        close = False
+        if data_file.fh is None:
+            close = True
+            data_file.open()
+        fh = data_file.fh['Forests']
+        if self.arbor._aos:
+            fh = fh['halos']
+
+        if root_only:
+            index = (root_node._si, root_node._si+1)
+        else:
+            index = (root_node._si, root_node._ei)
+
+        field_cache = data_file._field_cache
+        field_data = dict((field, field_cache.get(fh, field, index))
+                          for field in fields)
+
+        if close:
+            data_file.close()
+
+        fi = self.arbor.field_info
+        for field in fields:
+            units = fi[field].get("units", "")
+            if close:
+                field_data[field] = field_data[field].copy()
+            if units != "":
+                field_data[field] = \
+                  self.arbor.arr(field_data[field], units)
+
+        return field_data
+
+class TreeFrogRootFieldIO(DefaultRootFieldIO):
+    """
+    Read in fields for first node in all trees/forest.
+
+    This function is optimized for the struct of arrays layout.
+    It will work for array of structs layout, but field access
+    will be 1 to 2 orders of magnitude slower.
+    """
+    def _read_fields(self, storage_object, fields, dtypes=None):
+        if dtypes is None:
+            dtypes = {}
+
+        arbor = self.arbor
+        arbor._plant_trees()
+
+        # Use the _node_io_loop machinery to get the data files
+        # and corresponding tree indices.
+        data_files, index_list, _ = arbor._node_io_loop_prepare(None)
+
+        c = 0
+        rdata = defaultdict(list)
+
+        iend = arbor._file_count.cumsum()
+        istart = iend - arbor._file_count
+
+        pbar = get_pbar('Reading root fields', arbor.size)
+        for idf, (data_file, nodes) in enumerate(zip(data_files, index_list)):
+            my_indices = arbor._node_info['_si'][istart[idf]:iend[idf]]
+            arbor._node_io_loop_start(data_file)
+
+            fh = data_file.fh['Forests']
+            if self.arbor._aos:
+                fh = fh['halos']
+
+            for field in fields:
+                darray = fh[field][()]
+                rdata[field].append(darray[my_indices])
+
+            arbor._node_io_loop_finish(data_file)
+
+            c += my_indices.size
+            pbar.update(c)
+        pbar.finish()
+
+        field_data = {}
+        fi = self.arbor.field_info
+        for field in fields:
+            data = np.concatenate(rdata[field])
+            dtype = dtypes.get(field)
+            if dtype is not None:
+                data = data.astype(dtype)
+            units = fi[field].get("units", "")
+            if units != "":
+                data = self.arbor.arr(data, units)
+            field_data[field] = data
+
+        return field_data
