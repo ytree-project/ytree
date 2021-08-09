6ae3e830bc19c7fd477c317bc1c145c0634416e8
Britton Smith <brittonsmith@gmail.com>
2021-08-09 13:54:01 +0100

    Updates for use within ytree.

--- ytree/analysis/analysis_operators.py
+++ ytree/analysis/analysis_operators.py
@@ -1,5 +1,5 @@
 """
-callbacks, filters, quantities, and recipes for AnalysisPipeline
+AnalysisPipeline operators
 
 
 
@@ -10,21 +10,19 @@ import copy
 class OperatorRegistry(dict):
     def find(self, op, *args, **kwargs):
         if isinstance(op, str):
-            # Lookup, assuming string or hashable object
             op = copy.deepcopy(self[op])
             op.args = args
             op.kwargs = kwargs
         return op
 
+operation_registry = OperatorRegistry()
 
-callback_registry = OperatorRegistry()
+def add_operation(name, function):
+    operation_registry[name] =  AnalysisOperation(function)
 
-def add_callback(name, function):
-    callback_registry[name] =  AnalysisCallback(function)
-
-class AnalysisCallback(object):
+class AnalysisOperation(object):
     r"""
-    An AnalysisCallback is a function that minimally takes in a target object
+    An AnalysisOperation is a function that minimally takes in a target object
     and performs some analysis on it. This function may attach attributes
     to the target object, write out data, etc, but does not return anything.
     """
@@ -44,31 +42,15 @@ filter_registry = OperatorRegistry()
 def add_filter(name, function):
     filter_registry[name] = AnalysisFilter(function)
 
-class AnalysisFilter(AnalysisCallback):
+class AnalysisFilter(AnalysisOperation):
     r"""
     An AnalysisFilter is a function that minimally takes a target object, performs
     some analysis, and returns either True or False. The return value determines
     whether analysis is continued.
     """
     def __init__(self, function, *args, **kwargs):
-        AnalysisCallback.__init__(self, function, args, kwargs)
+        AnalysisOperation.__init__(self, function, args, kwargs)
 
-    def __call__(self, target):
-        return self.function(target, *self.args, **self.kwargs)
-
-quantity_registry = OperatorRegistry()
-
-def add_quantity(name, function):
-    quantity_registry[name] = AnalysisQuantity(function)
-
-class AnalysisQuantity(AnalysisCallback):
-    r"""
-    An AnalysisQuantity is a function that takes minimally a target object,
-    performs some analysis, and then returns a value.
-    """
-    def __init__(self, function, *args, **kwargs):
-        AnalysisCallback.__init__(self, function, args, kwargs)
-        
     def __call__(self, target):
         return self.function(target, *self.args, **self.kwargs)
 
@@ -80,7 +62,8 @@ def add_recipe(name, function):
 class AnalysisRecipe(object):
     r"""
     An AnalysisRecipe is a function that accepts an AnalysisPipeline and
-    adds a series of callbacks, quantities, and filters.
+    adds a series of operations and filters. This is a way of creating a
+    shortcut for a series of operations.
     """
     def __init__(self, function, args=None, kwargs=None):
         self.function = function
