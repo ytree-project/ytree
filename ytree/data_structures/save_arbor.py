c76445332abfa3fe0003f741daab207f2b8f1df1
Britton Smith <brittonsmith@gmail.com>
2022-07-11 07:30:26 -0500

    Add ability to only save files containing trees that need saving.

--- ytree/data_structures/save_arbor.py
+++ ytree/data_structures/save_arbor.py
@@ -166,11 +166,21 @@ def save_data_files(arbor, filename, fields, trees,
         save_size = len(trees)
 
     if update:
+        # Transplant field data onto tree roots.
         save_roots = transplant_analysis_fields(arbor, save_trees)
+        # If we have new analysis fields, they must be written for
+        # the entire arbor.
+        save_all = any([arbor.field_info[field]["type"] == "analysis"
+                        for field in fields])
     else:
         save_roots = {}
+        save_all = True
 
-    root_field_data = dict((field, []) for field in fields)
+    # This is the set of files we need to save.
+    if not save_all:
+        save_files = set([tree._ai for tree in save_roots.values()])
+
+    root_field_data = {field: [] for field in fields}
 
     group_nnodes = []
     group_ntrees = []
@@ -183,10 +193,18 @@ def save_data_files(arbor, filename, fields, trees,
         group_ntrees.append(cg_ntrees)
 
         total_guess = int(np.round(save_size * cg_number / sum(group_ntrees)))
-        save_data_file(
-            arbor, filename, fields,
-            np.array(current_group), root_field_data,
-            cg_number, total_guess)
+
+        # If we don't need to save the data file, just gather root fields.
+        if not save_all and cg_number-1 not in save_files:
+            for field in fields:
+                root_field_data[field].append(
+                    uconcatenate([[node[field] for node in current_group]]))
+
+        else:
+            save_data_file(
+                arbor, filename, fields,
+                np.array(current_group), root_field_data,
+                cg_number, total_guess)
 
     if update:
         file_sizes = np.diff(arbor._node_io._ei, prepend=0)
