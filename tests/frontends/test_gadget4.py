7f337944cd1f5f710bbcbe136efa3ce10271c999
Britton Smith <brittonsmith@gmail.com>
2022-07-14 10:18:34 -0500

    Add multi-file tests.

--- tests/frontends/test_gadget4.py
+++ tests/frontends/test_gadget4.py
@@ -4,9 +4,16 @@ from ytree.utilities.testing import \
     ArborTest, \
     TempDirTest
 
-class Gadget4ArborTest(TempDirTest, ArborTest):
+class Gadget4ArborSingleTest(TempDirTest, ArborTest):
     arbor_type = Gadget4Arbor
-    test_filename = "gadget4/trees.hdf5"
+    test_filename = "gadget4/trees/trees.hdf5"
     groups = ("forest", "tree", "prog")
     num_data_files = 1
+    tree_skip = 100
+
+class Gadget4ArborMultipleTest(TempDirTest, ArborTest):
+    arbor_type = Gadget4Arbor
+    test_filename = "gadget4/treedata/trees.0.hdf5"
+    groups = ("forest", "tree", "prog")
+    num_data_files = 64
     tree_skip = 100
