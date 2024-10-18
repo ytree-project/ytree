dbebfb9329a2b1d49d68da619c78f638a6c428e9
Britton Smith <brittonsmith@gmail.com>
2024-10-18 11:49:19 +0100

    Declare support for python 3.12.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(name="ytree",
           "Programming Language :: Python :: 3.9",
           "Programming Language :: Python :: 3.10",
           "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12",
       ],
       install_requires=[
           'configparser',
