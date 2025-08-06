c3b8ae43124aef19dc35fa210a0908dfe520ec8c
Britton Smith <brittonsmith@gmail.com>
2025-08-06 17:00:59 +0100

    Add test for AHF data with nonstandard naming conventions.

--- tests/frontends/test_ahf.py
+++ tests/frontends/test_ahf.py
@@ -16,3 +16,13 @@ class AHFNewArborTest(TempDirTest, ArborTest):
     test_filename = "AHF_100_tiny/GIZMO-NewMDCLUSTER_0047.snap_128.parameter"
     num_data_files = 5
     tree_skip = 100
+
+class AHFNameConfigArborTest(TempDirTest, ArborTest):
+    arbor_type = AHFArbor
+    test_filename = "B25_N256_CDM_1LPT/AHF.B25_N256_CDM_1LPT.snap_055.parameter"
+    load_kwargs = {"name_config":
+                       {"ahf_prefix": "AHF.B25_N256_CDM_1LPT",
+                        "mtree_prefix": "MTREE.B25_N256_CDM_1LPT.z39_adapt",
+                        "mtree_suffix": ".AHF_croco"}}
+    num_data_files = 2
+    tree_skip = 100
