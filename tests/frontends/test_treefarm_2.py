ce4243652144755103774d8b92027fd1aea2f002
Britton Smith <brittonsmith@gmail.com>
2025-08-13 10:01:34 +0100

    Split treefarm tests.

---
+++ tests/frontends/test_treefarm_2.py
@@ -0,0 +1,10 @@
+from ytree.frontends.treefarm import \
+    TreeFarmArbor
+from ytree.utilities.testing import \
+    ArborTest, \
+    TempDirTest
+
+class TreeFarmArborAncestorsTest(TempDirTest, ArborTest):
+    arbor_type = TreeFarmArbor
+    test_filename = "tree_farm/tree_farm_ancestors/fof_subhalo_tab_017.0.h5"
+    num_data_files = 34
