cbf5644b8f9afa4be57dfeca6c37b3ebf2b5e280
Orca <s2080293@ed.ac.uk>
2026-06-20 18:00:55 +0900

    Add test scaffolding for merger accretion rates example

--- doc/source/examples/merger_accretion_rates.py
+++ doc/source/examples/merger_accretion_rates.py
@@ -8,11 +8,28 @@ The growth rate of a halo is decomposed into two components:
 For each halo node, the rates are computed as:
     merger_rate = (sum of all progenitor masses - main progenitor mass) / dt
     smooth_accretion_rate = (total mass change - merger contribution) / dt
+
+NOTE: this script includes extra code to make it run within
+the test suite. To run conventionally, remove the following
+lines and return the code block in the middle to the proper
+tabbing (i.e., 4 spaces to the left).
+
+    from mpi4py import MPI
+    comm = MPI.Comm.Get_parent()
+    try:
+
+    except BaseException:
+        pass
+    comm.Barrier()
+    comm.Disconnect()
+
 """
 
 import ytree
 import yt
+
 yt.enable_parallelism()
+
 
 def calc_accretion_rates(node):
     ancestors = list(node.ancestors)
@@ -35,20 +52,32 @@ def calc_accretion_rates(node):
     node["mdot_merger"] = dm_merger / dt
     node["mdot_accretion"] = (dm_total - dm_merger) / dt
 
+
 if __name__ == "__main__":
-    a = ytree.load("tiny_ctrees/locations.dat")
-    a.add_analysis_field("mdot_merger", units="Msun/yr", default=0.)
-    a.add_analysis_field("mdot_accretion", units="Msun/yr", default=0.)
+    # Remove the next three and final four lines to run conventionally.
+    from mpi4py import MPI
 
-    ap = ytree.AnalysisPipeline()
-    ap.add_operation(calc_accretion_rates)
+    comm = MPI.Comm.Get_parent()
+    try:
+        a = ytree.load("tiny_ctrees/locations.dat")
+        a.add_analysis_field("mdot_merger", units="Msun/yr", default=0.)
+        a.add_analysis_field("mdot_accretion", units="Msun/yr", default=0.)
 
-    trees = list(a[:])
-    for tree in ytree.parallel_nodes(trees, filename="accretion_rates"):
-        yt.mylog.info(f"Processing {tree}.")
-        ap.process_target(tree)
+        ap = ytree.AnalysisPipeline()
+        ap.add_operation(calc_accretion_rates)
 
-    if yt.is_root():
-        a2 = ytree.load("accretion_rates/accretion_rates.h5")
-        print(a2[0]["mdot_merger"])
-        print(a2[0]["mdot_accretion"])
+        trees = list(a[:])
+        for tree in ytree.parallel_nodes(trees, filename="accretion_rates"):
+            yt.mylog.info(f"Processing {tree}.")
+            ap.process_target(tree)
+
+        if yt.is_root():
+            a2 = ytree.load("accretion_rates/accretion_rates.h5")
+            print(a2[0]["mdot_merger"])
+            print(a2[0]["mdot_accretion"])
+
+    # Remove the next four lines to run conventionally.
+    except BaseException:
+        pass
+    comm.Barrier()
+    comm.Disconnect()
